52f0202b861318d2e3ae4862e166cdd4d44f61d6
Soom350 <omaiga489@gmail.com>
2025-11-10 13:52:21 +0900

    追加

--- app/environmentInput.tsx
+++ app/environmentInput.tsx
@@ -15,9 +15,6 @@ export default function EnvironmentInput() {
     const [co2,setCo2] = useState(350);
 
     const handleSubmit = () =>{
-        if(!validateTitle){
-            return;
-        }
         const formData =[
             title,
             temperature,
@@ -37,12 +34,8 @@ export default function EnvironmentInput() {
     const validateTitle = () =>{
         if(!title){
             setTitleError("タイトルを入力してください");
-            return false;
         }else if(title.length <= 20){
-            setTitle("タイトルは20文字以内で入力してください");
-            return false;
-        }else{
-            return true;
+            setTitle("タイトルは20文字以内で入力してください")
         }
     }
 
